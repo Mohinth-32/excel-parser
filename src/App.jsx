b846a35356e3e3ffc7bb5ebfc95d2c19246ba788
Mohinth-32 <727821tuit101@skct.edu.in>
2026-01-02 18:10:44 +0530

    tracked enabled

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { useEffect } from 'react'
 import './App.css'
 import FileParser from './FileParser'
+import { trackPageView } from './tracking.js'
 
 function App() {
+  useEffect(() => {
+    // Track page view after component mounts
+    trackPageView();
+  }, []);
+
   return (
     <div className="app">
       <FileParser />
